93c8c90eb299ab24a4820675991330c5aea35728
gykim <rmsdud2435@naver.com>
2021-04-21 16:05:57 +0900

    start project

---
+++ src/main/java/com/springboot/gykim/springbootjpagaradle/SpringbootJpaGaradleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.gykim.springbootjpagaradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaGaradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaGaradleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springboot/gykim/springbootjpagaradle/configures/web/Pageable.java
@@ -0,0 +1,8 @@
+package com.springboot.gykim.springbootjpagaradle.configures.web;
+
+public interface Pageable {
+
+  long getOffset();
+  int getSize();
+
+}
